48d60aa23b06c6eddf4a07c5240daf1682df6908
Ivan Belyj <ivanbelyj743@gmail.com>
2022-07-11 17:23:55 +0300

    Add db initialization with roles and owner's account

---
+++ AvoskaIsReal/DbInitializer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using AvoskaIsReal.Domain;
+namespace AvoskaIsReal
+{
+    public class DbInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            // Add roles
+            await HasRole(roleManager, "moderator");
+            await HasRole(roleManager, "admin");
+            await HasRole(roleManager, "owner");
+
+            // Create owner's account
+            string ownerName = configuration.GetSection("Project")["OwnerUserName"];
+            string ownerPassword =
+                configuration.GetSection("Project")["OwnerInitialPassword"];
+            User owner = new User()
+            {
+                UserName = ownerName
+            };
+            IdentityResult createRes =
+                await userManager.CreateAsync(owner, ownerPassword);
+            if (createRes.Succeeded)
+            {
+                await userManager.AddToRoleAsync(owner, "owner");
+            }
+            else
+            {
+                // Todo: log error
+            }
+        }
+
+        private static async Task HasRole(RoleManager<IdentityRole> roleManager,
+            string roleName)
+        {
+            if (await roleManager.FindByNameAsync(roleName) == null)
+            {
+                IdentityResult createRes =
+                    await roleManager.CreateAsync(new IdentityRole(roleName));
+                if (createRes.Errors.Count() != 0)
+                {
+                    // Todo: log error
+                }
+            }
+        }
+    }
+}
--- AvoskaIsReal/Program.cs
+++ AvoskaIsReal/Program.cs
@@ -1,6 +1,7 @@
-using AvoskaIsReal.Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AvoskaIsReal.Domain;
+using AvoskaIsReal;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,17 +11,38 @@ builder.Services.AddDbContext<AppDbContext>((options) =>
 {
     options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 11)));
 });
-builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredUniqueChars = 1;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Initialize Db
+using (IServiceScope scope = app.Services.CreateScope())
+{
+    IServiceProvider services = scope.ServiceProvider;
+    UserManager<User> userManager =
+        (UserManager<User>)services.GetRequiredService(typeof(UserManager<User>));
+    RoleManager<IdentityRole> roleManager = (RoleManager<IdentityRole>)services
+        .GetRequiredService(typeof(RoleManager<IdentityRole>));
+    await DbInitializer.InitializeAsync(userManager, roleManager, app.Configuration);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios,
+    // The default HSTS value is 30 days.
+    // You may want to change this for production scenarios,
     // see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
